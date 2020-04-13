0536c44ff61fefb66b2a89b774f1f7a372857cfa
Maxim <mark.hruzin@gmail.com>
2020-04-13 12:26:59 +0400

    Add slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function () {
 
+    $('.slider__inner').slick({
+        nextArrow: "<button type='button' class='slick-btn slick-next'></button>",
+        prevArrow: "<button type='button' class='slick-btn slick-prev'></button>",
+        infinite: false
+    })
+
 });
